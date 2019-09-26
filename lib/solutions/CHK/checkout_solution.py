fc31057de1dfd9f5dbc6585331eff355368ea059
sbx_user1051 <sbx_user1051@169.254.108.13>
2019-09-26 23:16:50 +0000

    Thu Sep 26 23:16:50 UTC 2019

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -3,8 +3,21 @@
 # noinspection PyUnusedLocal
 # skus = unicode string
 def checkout(skus):
-    skuarr = [sku for sku in skus]
     na, nb, nc, nd = 0, 0, 0, 0
+    for sku in skus:
+        if 'A' == sku:
+            na += 1
+        elif 'B' == sku:
+            nb += 1
+        elif 'C' == sku:
+            nc += 1
+        elif 'D' == sku:
+            nd += 1
+    pa = int(na / 3) * 130 + (na % 3) * 50
+    pb = int(nb / 2) * 45 + (nb % 2) * 30
+    pc = 20 * nc
+    pd = 10 * nd
+    return na + nb + nc + nd
 
 
 
