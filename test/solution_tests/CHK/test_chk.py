ac7ccf4b191d89ef578032cc6866c8eb82f0ff1e
sbx_user1051 <sbx_user1051@169.254.108.13>
2019-09-26 23:35:12 +0000

    Thu Sep 26 23:35:12 UTC 2019

--- test/solution_tests/CHK/test_chk.py
+++ test/solution_tests/CHK/test_chk.py
@@ -8,4 +8,8 @@ class TestSum(TestCase):
     def test_chkE(self):
         self.assertEqual(checkout_solution.checkout('BBE'), 85)
 
+    def test_chkEE(self):
+        self.assertEqual(checkout_solution.checkout('BBEE'), 110)
+
+
 
