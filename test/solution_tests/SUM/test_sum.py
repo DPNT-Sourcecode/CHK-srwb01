750761a67558d9134810d6de820ebbb8742d1dc1
sbx_user1051 <sbx_user1051@169.254.108.13>
2019-09-26 18:26:47 +0000

    Thu Sep 26 18:26:47 UTC 2019

--- test/solution_tests/SUM/test_sum.py
+++ test/solution_tests/SUM/test_sum.py
@@ -1,6 +1,34 @@
 from solutions.SUM import sum_solution
 
+from unittest import TestCase
 
-class TestSum():
+
+class TestSum(TestCase):
+
     def test_sum(self):
-        assert sum_solution.compute(1, 2) == 3
+        self.assertEqual(sum_solution.compute(1, 2), 3)
+
+    def test_out_of_bounds_s1_s2(self):
+        with self.assertRaises(ValueError):
+            sum_solution.compute(-1, -1)
+
+    def test_out_of_bounds_s1(self):
+        with self.assertRaises(ValueError):
+            sum_solution.compute(-1, 2)
+
+    def test_out_of_bounds_s2(self):
+        with self.assertRaises(ValueError):
+            sum_solution.compute(2, -1)
+
+    def test_out_of_bounds_b1_b2(self):
+        with self.assertRaises(ValueError):
+            sum_solution.compute(101, 101)
+
+    def test_out_of_bounds_b1(self):
+        with self.assertRaises(ValueError):
+            sum_solution.compute(101, 101)
+
+    def test_out_of_bounds_b2(self):
+        with self.assertRaises(ValueError):
+            sum_solution.compute(101, 101)
+
