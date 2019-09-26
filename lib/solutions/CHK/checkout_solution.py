af6b51330796c296666f5e802163bf76bc0988ff
sbx_user1051 <sbx_user1051@169.254.108.13>
2019-09-26 23:13:50 +0000

    Thu Sep 26 23:13:50 UTC 2019

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -3,4 +3,8 @@
 # noinspection PyUnusedLocal
 # skus = unicode string
 def checkout(skus):
-    raise NotImplementedError()
+    skuarr = [sku for sku in skus]
+    na, nb, nc, nd = 0, 0, 0, 0
+
+
+
