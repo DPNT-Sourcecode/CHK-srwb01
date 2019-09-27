3e69da288edc28828c157037721b4a9a13038fca
sbx_user1051 <sbx_user1051@169.254.108.13>
2019-09-27 00:03:25 +0000

    Fri Sep 27 00:03:25 UTC 2019

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -3,18 +3,19 @@
 # noinspection PyUnusedLocal
 # skus = unicode string
 def checkout(skus):
-    skudict = dict()
+    sd = dict()
     for sku in skus:
         if sku in 'ABCDEFGHIJKLMNOPQRSTUVWXYZ':
-            if sku in skudict:
-                skudict[sku] += 1
+            if sku in sd:
+                sd[sku] += 1
             else:
-                skudict[sku] = 1
+                sd[sku] = 1
         else:
             return -1
-    pa = int(na / 5) * 200
-    na %= 5
-    pa += int(na / 3) * 130 + (na % 3) * 50
+
+    pa = int(sd['A'] / 5) * 200
+    sd['A'] %= 5
+    pa += int(sd['A'] / 3) * 130 + (sd['A'] % 3) * 50
     pc = 20 * nc
     pd = 15 * nd
     pe = 40 * ne
@@ -24,5 +25,6 @@ def checkout(skus):
     nf = max(0, nf-ff)
     pf = 10 * nf
     return int(pa + pb + pc + pd + pe + pf)
+
 
 
